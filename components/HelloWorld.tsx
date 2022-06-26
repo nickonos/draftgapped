511625f8ffa0ea2ad2f57b99242a659c21114237
nickonos <nick.van.oijen@gmail.com>
2022-06-26 15:29:46 +0200

    Setup next app with ts and tailwind

---
+++ components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HelloWorld(){
+    return (
+        <div className="flex justify-center items-center h-screen h-screen">
+            <div className="text-3xl text-center w-fit h-fit">
+                Hello world!
+            </div>
+        </div>
+    )
+}
